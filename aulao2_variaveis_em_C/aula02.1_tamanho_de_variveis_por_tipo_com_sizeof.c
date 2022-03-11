5e403af5af4046254d796ad4447c6c117b60f577
root <root@DESKTOP-L560ESN.localdomain>
2022-03-11 13:45:46 -0300

    Usando variaveis

---
+++ aulao2_variaveis_em_C/aula02.1_tamanho_de_variveis_por_tipo_com_sizeof.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+
+  printf("The variables's size in C are:\n Char: %lu\n Unsigned Char: %lu\n Short: %lu\n Unsigned Short: %lu\n Int: %lu\n", sizeof (char), sizeof (unsigned char), sizeof (short), sizeof (unsigned short), sizeof (int));
+}           
---
+++ aulao2_variaveis_em_C/char.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+  char c;
+
+ printf("O tamanho da variavel char c eh: %lu\n", sizeof c);
+
+ return 0;
+}
