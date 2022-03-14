056c6adfbc5e51fd0d1f27484e987d7f47d625a5
Fabiano <fabiano.alexandred@gmail.com>
2022-03-14 01:43:14 -0300

    aula 01

---
+++ aula01/hello.c
@@ -0,0 +1,5 @@
+#include <stdio.h> //Biblioteca para chamada e execução do printf
+
+int main(void){ // Função principal de execução em C
+  printf("Hello C"); // imprimindo a frase hello C na tela.
+} //Chave de fechamento da função principal
