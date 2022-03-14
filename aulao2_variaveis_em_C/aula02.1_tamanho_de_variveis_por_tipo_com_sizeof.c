d5d8242c77237091da5451c3c8c407b321e9e12b
Fabiano <fabiano.alexandred@gmail.com>
2022-03-14 01:36:51 -0300

    Inserindo aula 01 e refinado aula 02

--- aulao2_variaveis_em_C/aula02.1_tamanho_de_variveis_por_tipo_com_sizeof.c
+++ aulao2_variaveis_em_C/aula02.1_tamanho_de_variveis_por_tipo_com_sizeof.c
@@ -2,5 +2,5 @@
 
 int main(void){
 
-  printf("The variables's size in C are:\n Char: %lu\n Unsigned Char: %lu\n Short: %lu\n Unsigned Short: %lu\n Int: %lu\n", sizeof (char), sizeof (unsigned char), sizeof (short), sizeof (unsigned short), sizeof (int));
+  printf("The variables's size in C are:\n Char: byte = %lu bits = byte * 8 = 8\n Unsigned Char: %lu\n Short: %lu\n Unsigned Short: %lu\n Unsigned: %lu\n Int: %lu\n Float: %lu\n Double: %lu\n Long: %lu\n Unsigned Long: %lu\n Long Double: %lu\n", sizeof (char), sizeof (unsigned char), sizeof (short), sizeof (unsigned short), sizeof (unsigned), sizeof (int), sizeof (float), sizeof (double), sizeof (long), sizeof (unsigned long), sizeof (long double));
 }           
