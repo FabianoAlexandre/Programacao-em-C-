3e08bfbcc07ed1c48d0455a19e2deb3704399423
Fabiano <fabiano.alexandred@gmail.com>
2022-04-03 21:05:39 -0300

    Create class 4 - working with unsigned variables and float variables

---
+++ aula04_variaveis_unsigned/Variables_Unsigned.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdint.h> //
+
+int main(void){
+   unsigned int = UINT_MAX;
+  printf("The size of unsigned int is: %ud\n", sizeof);
+
+}
---
+++ aula04_variaveis_unsigned/float.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int main(void){
+ 
+  float f = 1;//C aceita expressões matematicas como 3e2 = 3*10^2 = 300
+  double d = 1;  
+  long double ld = 1;
+
+  printf("O tamanho de f (float): %zu bytes / %zu bits\n", sizeof f, sizeof f * 8);
+ 
+  printf("O tamanho de d (double): %zu bytes / %zu bits\n", sizeof d, sizeof d * 8);
+  
+  printf("O tamanho de ld (long double): %zu bytes / %zu bits\n", sizeof ld, sizeof ld * 8);
+
+  printf("Valor de ld: %.2f\n", f);//%.2f indica a quantidade de casas decimais, (2), após a virgula.
+
+  return 0;
+}
