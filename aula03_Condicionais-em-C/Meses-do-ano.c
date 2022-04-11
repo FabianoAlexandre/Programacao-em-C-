fd9cd780d1ff8f277c6161479c5d902469bd9812
Fabiano <fabiano.alexandred@gmail.com>
2022-04-11 15:53:54 -0300

    inserindo aula03 trabalhando com condicionais if e else

---
+++ aula03_Condicionais-em-C/Meses-do-ano.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main(void){
+  int mes = 6;
+
+  if(mes == 1){
+    printf("Janeiro\n");
+  }
+  else if(mes == 2){
+    printf("fevereiro\n");
+  }
+  else if(mes == 3){
+    printf("Março\n");
+  }
+  else if(mes == 4){
+    printf("Abril\n");
+  }
+  else if(mes == 5){
+    printf("Maio\n");
+  }
+  else if(mes == 6){
+    printf("Junho\n");
+  }
+  else if(mes == 7){
+    printf("Julho\n");
+  }
+  else if(mes == 8){
+    printf("Agosto\n");
+  }
+  else if(mes == 9){
+    printf("Setembro\n");
+  }
+  else if(mes == 10){
+    printf("Outubro");
+  }
+  else if(mes == 11){
+    printf("Novembro\n");
+  }
+  else if(mes == 12){
+    printf("Dezembro\n");
+  }
+  return 0;
+}  
+
+
+   
---
+++ aula03_Condicionais-em-C/Permissao-para-Dirigir.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+
+  int idade = 18;
+
+  if(idade < 18){
+  printf("Você Ainda Não Pode Tirar a CNH\n");
+  }
+  else {
+    printf("Você tem permissão para tirar sua CNH e dirigir\n");
+  }
+
+  return 0;
+} 
